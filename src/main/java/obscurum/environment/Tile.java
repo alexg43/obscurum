4bca11df68bd74952b18add3bc5a4fab7fe51c88
Alex <alexghita96@gmail.com>
2020-06-07 17:33:20 +0100

    refactor: validate only in setter

--- src/main/java/obscurum/environment/Tile.java
+++ src/main/java/obscurum/environment/Tile.java
@@ -4,26 +4,27 @@ import java.awt.Color;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 import obscurum.GameMain;
 
 @Getter
 public abstract class Tile {
     protected String name;
     protected char glyph;
-    @Setter protected Color foregroundColour;
+    protected Color foregroundColour;
 
     public Tile(String name, char glyph, Color foregroundColour) {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty.");
         } else if (glyph >= GameMain.NUM_OF_GLYPHS) {
             throw new IllegalArgumentException(String.format("Glyph must be in range 0 - %d.", GameMain.NUM_OF_GLYPHS));
-        } else if (foregroundColour == null) {
-            throw new IllegalArgumentException("Foreground colour cannot be null.");
         }
 
         this.name = name;
         this.glyph = glyph;
+        setForegroundColour(foregroundColour);
+    }
+
+    public void setForegroundColour(@NonNull Color foregroundColour) {
         this.foregroundColour = foregroundColour;
     }
 
